8025516d3e796c3aa227f69b3b93b0bed6eaed9f
kamini-pal <kamini9926@gmail.com>
2026-09-05 19:32:25 +0530

    create project uing vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Hello Kamini</h1>
+      <h2>Role based Navigation</h2>
+      <ul>
+        <li></li>
+      </ul>
+    </>
+  )
+}
+
+export default App
